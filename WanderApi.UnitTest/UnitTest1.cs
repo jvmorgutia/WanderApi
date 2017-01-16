e8062210aeb8baad7efc082d69d6c35738b36a9f
Jesse Morgutia <jvmorgutia@gmail.com>
2017-01-16 14:04:15 -0800

    Add project files.

---
+++ WanderApi.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WanderApi.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
